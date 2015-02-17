fb99591613eff4fd4d33b289c70ae7b8b44bcb8b
jaywcjlove <398188662@qq.com>
2015-02-17 23:41:00 +0800

    tab空格

--- online.js
+++ online.js
@@ -1,28 +1,28 @@
 ;(function(w){
     var xmlhttp = new XMLHttpRequest(),
-    	time=2000,//设置轮询时间
-    	url="http://********.com/online.php";
-    	//当 `url`为空的时候 默认所有浏览器使用 onLine 和 offline事件
+        time=2000,//设置轮询时间
+        url="http://********.com/online.php";
+        //当 `url`为空的时候 默认所有浏览器使用 onLine 和 offline事件
 
     w.onlinenetwork = w.onlinenetwork || {};
-	w.onlinenetwork = {
-		setStatus:function (newStatus) {
-			this.eventStatus(newStatus);
-			w.onLine = newStatus;
-		},
-		//状态改变执行事件
-		eventStatus: function (newStatus) {
-			if (newStatus === true && w.onLineHandler !== undefined && (w.onLine !== true || this.handlerFired === false)) {
-				w.onLineHandler();
-			}
-			if (newStatus === false && w.offLineHandler !== undefined && (w.onLine !== false || this.handlerFired === false)) {
-			    w.offLineHandler();
-			}
-			this.handlerFired = true;
-		},
-		//http请求
-		XMLHttpLogic:function (async) {
-			var url = this.getOnLineCheckURL(),
+    w.onlinenetwork = {
+        setStatus:function (newStatus) {
+            this.eventStatus(newStatus);
+            w.onLine = newStatus;
+        },
+        //状态改变执行事件
+        eventStatus: function (newStatus) {
+            if (newStatus === true && w.onLineHandler !== undefined && (w.onLine !== true || this.handlerFired === false)) {
+                w.onLineHandler();
+            }
+            if (newStatus === false && w.offLineHandler !== undefined && (w.onLine !== false || this.handlerFired === false)) {
+                w.offLineHandler();
+            }
+            this.handlerFired = true;
+        },
+        //http请求
+        XMLHttpLogic:function (async) {
+            var url = this.getOnLineCheckURL(),
                 self = this;
             if(async){
                 xmlhttp.onreadystatechange = function () {
@@ -37,9 +37,9 @@
             }else{
                 xmlhttp.onreadystatechange = undefined;
             }
-			xmlhttp.open("HEAD", url, async)
+            xmlhttp.open("HEAD", url, async)
             this.tryToSend(xmlhttp);
-		},
+        },
         processXmlhttpStatus: function () {
             var tempOnLine = w.onlinenetwork.verifyStatus(xmlhttp.status);
             this.setStatus(tempOnLine);
@@ -56,14 +56,14 @@
         verifyStatus:function (status) {
             return status === 200;
         },
-		//url加上随机数
-		getOnLineCheckURL: function () {
-			return url + '?' + Math.floor(Math.random() * 1000000);
-		},
-		//非 chrome 和 Safari 浏览器不停的检查，嘿嘿
-		startCheck:function(){
-			setInterval("window.onlinenetwork.XMLHttpLogic(true)",time);
-		},
+        //url加上随机数
+        getOnLineCheckURL: function () {
+            return url + '?' + Math.floor(Math.random() * 1000000);
+        },
+        //非 chrome 和 Safari 浏览器不停的检查，嘿嘿
+        startCheck:function(){
+            setInterval("window.onlinenetwork.XMLHttpLogic(true)",time);
+        },
         //第一次检查是否在线
         checkOnLine:function(){
             this.XMLHttpLogic(false)
@@ -75,43 +75,43 @@
                 this.setStatus(true);
             }
         },
-		//判断浏览器
-		getExplorer: function(newStatus){
-			var explorer = window.navigator.userAgent;
+        //判断浏览器
+        getExplorer: function(newStatus){
+            var explorer = window.navigator.userAgent;
             this.setStatus(newStatus)
-			if((explorer.indexOf('Firefox') >= 0 || explorer.indexOf('MSIE') >= 0)&&url){
-				console.log("test:1")
+            if((explorer.indexOf('Firefox') >= 0 || explorer.indexOf('MSIE') >= 0)&&url){
+                console.log("test:1")
                 this.checkOnLine()
                 this.setStatus(newStatus)
                 this.startCheck(newStatus)
             }else{
-				console.log("test:2")
+                console.log("test:2")
                 this.eventStatus(newStatus)
-			}
-		},
-		//绑定事件
-		addEvent: function (obj, type, callback) {
-			if (window.attachEvent) obj.attachEvent('on' + type, callback);
-			else obj.addEventListener(type, callback);
-		},
-		init:function(){
+            }
+        },
+        //绑定事件
+        addEvent: function (obj, type, callback) {
+            if (window.attachEvent) obj.attachEvent('on' + type, callback);
+            else obj.addEventListener(type, callback);
+        },
+        init:function(){
 
-			var self = this
+            var self = this
             //获取当前状态
-			this.addEvent(w, 'load', function () {
+            this.addEvent(w, 'load', function () {
                 self.eventStatus(w.onLine);
             });
 
-			//侦听 online 事件
-			this.addEvent(w, 'online', function () { self.getExplorer(true) });
+            //侦听 online 事件
+            this.addEvent(w, 'online', function () { self.getExplorer(true) });
 
-			//侦听 offline 事件
-			this.addEvent(w, 'offline', function () { self.getExplorer(false) });
+            //侦听 offline 事件
+            this.addEvent(w, 'offline', function () { self.getExplorer(false) });
 
             self.getExplorer(true) 
-			this.handlerFired = false;
-		}
-	}
-	w.onlinenetwork.init()
-	
+            this.handlerFired = false;
+        }
+    }
+    w.onlinenetwork.init()
+    
 })(window);
